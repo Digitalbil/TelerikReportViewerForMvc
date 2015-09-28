f70dfadc1470210872a772f348a7e88cc8a79099
Antonio Feregrino <antonio.feregrino@gmail.com>
2015-09-28 15:19:07 -0500

    Telerik specific stuff.

--- ReportViewerForMvc/HtmlHelperExtensions.cs
+++ ReportViewerForMvc/HtmlHelperExtensions.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Reporting.WebForms;
+﻿using MsftReporting = Microsoft.Reporting.WebForms;
+using TkReporting = Telerik.ReportViewer.WebForms;
 using System.Web;
 using System.Web.Mvc;
 
@@ -15,7 +16,7 @@ namespace ReportViewerForMvc
         /// <param name="helper">The HTML helper instance that this method extends.</param>
         /// <param name="reportViewer">The object containing the ReportViewer control properties.</param>
         /// <returns>An HTML iframe that sets its heigh and width based on the content of the report.</returns>
-        public static HtmlString ReportViewer(this HtmlHelper helper, ReportViewer reportViewer)
+        public static HtmlString ReportViewer(this HtmlHelper helper, MsftReporting.ReportViewer reportViewer)
         {
             return ReportViewerForMvc.GetIframe(reportViewer, null);
         }
@@ -27,7 +28,7 @@ namespace ReportViewerForMvc
         /// <param name="reportViewer">The object containing the ReportViewer control properties.</param>
         /// <param name="htmlAttributes">The object containing the HTML attributes of the iframe.</param>
         /// <returns>An HTML iframe with the specified attributes that sets its heigh and width based on the content of the report.</returns>
-        public static HtmlString ReportViewer(this HtmlHelper helper, ReportViewer reportViewer, object htmlAttributes)
+        public static HtmlString ReportViewer(this HtmlHelper helper, MsftReporting.ReportViewer reportViewer, object htmlAttributes)
         {
             return ReportViewerForMvc.GetIframe(reportViewer, htmlAttributes);
         }
@@ -41,7 +42,7 @@ namespace ReportViewerForMvc
         /// <returns>An HTML iframe that sets its heigh and width based on the content of the report.</returns>
         public static HtmlString ReportViewer(this HtmlHelper helper, object reportViewer, object report)
         {
-            ReportViewer reportViewerControl = ReportViewerForMvc.AnonymousReportViewer(reportViewer, report);
+            MsftReporting.ReportViewer reportViewerControl = ReportViewerForMvc.AnonymousReportViewer(reportViewer, report);
 
             return ReportViewerForMvc.GetIframe(reportViewerControl, null);
         }
@@ -56,7 +57,7 @@ namespace ReportViewerForMvc
         /// <returns>An HTML iframe with the specified attributes that sets its heigh and width based on the content of the report.</returns>
         public static HtmlString ReportViewer(this HtmlHelper helper, object reportViewer, object report, object htmlAttributes)
         {
-            ReportViewer reportViewerControl = ReportViewerForMvc.AnonymousReportViewer(reportViewer, report);
+            MsftReporting.ReportViewer reportViewerControl = ReportViewerForMvc.AnonymousReportViewer(reportViewer, report);
 
             return ReportViewerForMvc.GetIframe(reportViewerControl, htmlAttributes);
         }
@@ -72,7 +73,7 @@ namespace ReportViewerForMvc
         /// <returns>An HTML iframe with the specified attributes that sets its heigh and width based on the content of the report.</returns>
         public static HtmlString ReportViewer(this HtmlHelper helper, object reportViewer, object report, object parameters, object htmlAttributes)
         {
-            ReportViewer reportViewerControl = ReportViewerForMvc.AnonymousReportViewer(reportViewer, report, parameters);
+            MsftReporting.ReportViewer reportViewerControl = ReportViewerForMvc.AnonymousReportViewer(reportViewer, report, parameters);
 
             return ReportViewerForMvc.GetIframe(reportViewerControl, htmlAttributes);
         }
@@ -89,9 +90,96 @@ namespace ReportViewerForMvc
         /// <returns>An HTML iframe with the specified attributes that sets its heigh and width based on the content of the report.</returns>
         public static HtmlString ReportViewer(this HtmlHelper helper, object reportViewer, object report, object parameters, object dataSources, object htmlAttributes)
         {
-            ReportViewer reportViewerControl = ReportViewerForMvc.AnonymousReportViewer(reportViewer, report, parameters, dataSources);
+            MsftReporting.ReportViewer reportViewerControl = ReportViewerForMvc.AnonymousReportViewer(reportViewer, report, parameters, dataSources);
 
             return ReportViewerForMvc.GetIframe(reportViewerControl, htmlAttributes);
         }
+
+        #region Telerik report viewwers
+        /// <summary>
+        /// Returns an HTML iframe that renders an ASP.NET ReportViewer control.
+        /// </summary>
+        /// <param name="helper">The HTML helper instance that this method extends.</param>
+        /// <param name="reportViewer">The object containing the ReportViewer control properties.</param>
+        /// <returns>An HTML iframe that sets its heigh and width based on the content of the report.</returns>
+        public static HtmlString TelerikReportViewer(this HtmlHelper helper, TkReporting.ReportViewer reportViewer)
+        {
+            return TelerikReportViewerForMvc.GetIframe(reportViewer, null);
+        }
+
+        /// <summary>
+        /// Returns an HTML iframe that renders an ASP.NET ReportViewer control.
+        /// </summary>
+        /// <param name="helper">The HTML helper instance that this method extends.</param>
+        /// <param name="reportViewer">The object containing the ReportViewer control properties.</param>
+        /// <param name="htmlAttributes">The object containing the HTML attributes of the iframe.</param>
+        /// <returns>An HTML iframe with the specified attributes that sets its heigh and width based on the content of the report.</returns>
+        public static HtmlString TelerikReportViewer(this HtmlHelper helper, TkReporting.ReportViewer reportViewer, object htmlAttributes)
+        {
+            return TelerikReportViewerForMvc.GetIframe(reportViewer, htmlAttributes);
+        }
+
+        /// <summary>
+        /// Returns an HTML iframe that renders an ASP.NET ReportViewer control.
+        /// </summary>
+        /// <param name="helper">The HTML helper instance that this method extends.</param>
+        /// <param name="reportViewer">The object containing the ReportViewer control properties.</param>
+        /// <param name="report">An object containing the LocalReport/ServerReport properties.</param>
+        /// <returns>An HTML iframe that sets its heigh and width based on the content of the report.</returns>
+        public static HtmlString TelerikReportViewer(this HtmlHelper helper, object reportViewer, object report)
+        {
+            MsftReporting.ReportViewer reportViewerControl = ReportViewerForMvc.AnonymousReportViewer(reportViewer, report);
+
+            return ReportViewerForMvc.GetIframe(reportViewerControl, null);
+        }
+
+        /// <summary>
+        /// Returns an HTML iframe that renders an ASP.NET ReportViewer control.
+        /// </summary>
+        /// <param name="helper">The HTML helper instance that this method extends.</param>
+        /// <param name="reportViewer">The object containing the ReportViewer control properties.</param>
+        /// <param name="report">An object containing the LocalReport/ServerReport properties.</param>
+        /// <param name="htmlAttributes">The object containing the HTML attributes of the iframe.</param>
+        /// <returns>An HTML iframe with the specified attributes that sets its heigh and width based on the content of the report.</returns>
+        public static HtmlString TelerikReportViewer(this HtmlHelper helper, object reportViewer, object report, object htmlAttributes)
+        {
+            TkReporting.ReportViewer reportViewerControl = TelerikReportViewerForMvc.AnonymousReportViewer(reportViewer, report);
+
+            return TelerikReportViewerForMvc.GetIframe(reportViewerControl, htmlAttributes);
+        }
+
+        /// <summary>
+        /// Returns an HTML iframe that renders an ASP.NET ReportViewer control.
+        /// </summary>
+        /// <param name="helper">The HTML helper instance that this method extends.</param>
+        /// <param name="reportViewer">The object containing the ReportViewer control properties.</param>
+        /// <param name="report">An object containing the LocalReport/ServerReport properties.</param>
+        /// <param name="parameters">Object that contains the parameters for a report.</param>
+        /// <param name="htmlAttributes">The object containing the HTML attributes of the iframe.</param>
+        /// <returns>An HTML iframe with the specified attributes that sets its heigh and width based on the content of the report.</returns>
+        public static HtmlString TelerikReportViewer(this HtmlHelper helper, object reportViewer, object report, object parameters, object htmlAttributes)
+        {
+            TkReporting.ReportViewer reportViewerControl = TelerikReportViewerForMvc.AnonymousReportViewer(reportViewer, report, parameters);
+
+            return TelerikReportViewerForMvc.GetIframe(reportViewerControl, htmlAttributes);
+        }
+
+        /// <summary>
+        /// Returns an HTML iframe that renders an ASP.NET ReportViewer control.
+        /// </summary>
+        /// <param name="helper">The HTML helper instance that this method extends.</param>
+        /// <param name="reportViewer">The object containing the ReportViewer control properties.</param>
+        /// <param name="report">An object containing the LocalReport/ServerReport properties.</param>
+        /// <param name="parameters">Object that contains the parameters for a report.</param>
+        /// <param name="dataSources">The data sources to be added to the report.</param>
+        /// <param name="htmlAttributes">The object containing the HTML attributes of the iframe.</param>
+        /// <returns>An HTML iframe with the specified attributes that sets its heigh and width based on the content of the report.</returns>
+        public static HtmlString TelerikReportViewer(this HtmlHelper helper, object reportViewer, object report, object parameters, object dataSources, object htmlAttributes)
+        {
+            TkReporting.ReportViewer reportViewerControl = TelerikReportViewerForMvc.AnonymousReportViewer(reportViewer, report, parameters, dataSources);
+
+            return TelerikReportViewerForMvc.GetIframe(reportViewerControl, htmlAttributes);
+        }
+        #endregion
     }
 }
---
+++ ReportViewerForMvc/TelerikReportViewerWebForm.aspx
@@ -0,0 +1,27 @@
+﻿<%@ Page Language="C#" AutoEventWireup="True" CodeBehind="TelerikReportViewerWebForm.aspx.cs" Inherits="ReportViewerForMvc.TelerikReportViewerWebForm" %>
+
+<%@ Register Assembly="Microsoft.ReportViewer.WebForms, Version=11.0.0.0, Culture=neutral, PublicKeyToken=89845dcd8080cc91" Namespace="Microsoft.Reporting.WebForms" TagPrefix="rsweb" %>
+
+<%@ Register TagPrefix="telerik" Namespace="Telerik.ReportViewer.WebForms"
+        Assembly="Telerik.ReportViewer.WebForms, Version=7.2.14.127, Culture=neutral, PublicKeyToken=a9d7983dfcc261be" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body style="margin: 0px; padding: 0px;">
+    <form id="form1" runat="server">
+        <div>
+            <asp:ScriptManager ID="ScriptManager1" runat="server">
+                <Scripts>
+                    <asp:ScriptReference Assembly="ReportViewerForMvc" Name="ReportViewerForMvc.Scripts.PostMessage.js" />
+                </Scripts>
+            </asp:ScriptManager>
+            <telerik:ReportViewer ID="ReportViewer1" runat="server"></telerik:ReportViewer>
+            <%--<rsweb:ReportViewer ID="ReportViewer1" runat="server"></rsweb:ReportViewer>--%>
+        </div>
+    </form>
+</body>
+</html>
